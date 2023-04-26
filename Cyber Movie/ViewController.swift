08b63dec0e86be09dfac2acd03d3909b50a4c38a
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2023-04-26 21:01:14 +0300

    Initial Commit

---
+++ Cyber Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cyber Movie
+//
+//  Created by Дмитро Павлов on 26.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
