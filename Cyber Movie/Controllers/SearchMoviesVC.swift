c32da0ed4861aed781618e02b2a84564a94feabf
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2023-04-28 16:16:54 +0300

    feat: added simple table view and tested (CBR-5)

--- Cyber Movie/Controllers/SearchMoviesVC.swift
+++ Cyber Movie/Controllers/SearchMoviesVC.swift
@@ -8,13 +8,33 @@
 import UIKit
 
 class SearchMoviesVC: UIViewController {
+    @IBOutlet weak var searchMoviesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        searchMoviesTableView.dataSource = self
 
         setupUI()
     }
     
     func setupUI() {
         view.backgroundColor = Color.mainBG
+        
     }
 }
+
+extension SearchMoviesVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = "Test"
+        
+        return cell
+    }
+}
+
