a817d3199e83a3487a8b8a8560a663161b177a57
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2023-05-01 15:58:41 +0300

    fix: search with space in title was not working (CBR-9)

--- Cyber Movie/NetworkService.swift
+++ Cyber Movie/NetworkService.swift
@@ -13,8 +13,10 @@ final class NetworkService {
     private init() {}
     
     func searchFor<T: Codable>(model: T.Type, _ searchType: String, _ query: String,  completion: @escaping((T) -> ())) {
+        let encodedQuery = encodeQueryParam(query) ?? ""
+        
         let baseUrl: String = "\(Config.API_MOVIEDB_HOST)/3/search/\(searchType)"
-        let urlParams: String = "?api_key=\(Config.TMDB_API_KEY)&query=\(query)"
+        let urlParams: String = "?api_key=\(Config.TMDB_API_KEY)&query=\(encodedQuery)"
         let url: String = baseUrl + urlParams
         
         AF.request(url).responseDecodable(of: T.self) { response in
@@ -32,5 +34,9 @@ final class NetworkService {
             let moviesResponse = response.value
             completion(moviesResponse!)
         }
+    }
+    
+    private func encodeQueryParam(_ param: String) -> String? {
+        return param.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
     }
 }
