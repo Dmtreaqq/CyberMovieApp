ba07a0a73a9f4db262fa1eee9f73d61542313500
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2023-05-09 18:56:34 +0300

    feat: added alert in login screen

--- Cyber Movie/View/LoginVC.swift
+++ Cyber Movie/View/LoginVC.swift
@@ -26,7 +26,18 @@ class LoginVC: UIViewController {
         let sb = UIStoryboard(name: "Movies", bundle: nil)
         let vc = sb.instantiateViewController(withIdentifier: "CustomTabBarController")
         
-        navigationController?.pushViewController(vc, animated: true)
+        let isFieldsEmpty = checkEmptyFields()
+        
+        if isFieldsEmpty {
+            let alert = UIAlertController(title: "Warning", message: "Enter something in one of fields", preferredStyle: .alert)
+            
+            let action = UIAlertAction(title: "OK", style: .default)
+            
+            alert.addAction(action)
+            present(alert, animated: true)
+        } else {
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
     
     func setupUI() {
@@ -46,6 +57,22 @@ class LoginVC: UIViewController {
         
         loginImageView.image = UIImage(named: "LaunchLogo")
         loginImageView.tintColor = .white
+    }
+    
+    func checkEmptyFields() -> Bool {
+        guard let login = loginTextField.text else {
+            return true
+        }
+        
+        guard let password = passwordTextField.text else {
+            return true
+        }
+        
+        if password.isEmpty && login.isEmpty {
+            return true
+        }
+        
+        return false
     }
 }
 
